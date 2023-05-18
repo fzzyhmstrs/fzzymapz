6e946c8d5673051d260bc7a94e2ced5c0e27b411
fzzyhmstrs <72876796+fzzyhmstrs@users.noreply.github.com>
2023-05-18 09:25:40 -0400

    Update Tile.kt

--- src/main/kotlin/me/fzzyhmstrs/fzzymapz/tile/Tile.kt
+++ src/main/kotlin/me/fzzyhmstrs/fzzymapz/tile/Tile.kt
@@ -8,7 +8,11 @@ import net.minecraft.util.Identifier
 abstract class Tile(val texture: Identifier, val isEmpty: Boolean = false, val type: TileType){
 
       abstract fun draw(matrices: MatrixStack, x: Int, y: Int, hc: Boolean)
-
+      
+      open fun toJson(): JsonObject{
+            return type.saveTile(this)
+      }
+      
       object EMPTY: Tile(Identifier(FM.MOD_ID,"empty"),true, RegisterTile.EmptyType){
           override fun draw(matrices: MatrixStack, x: Int, y: Int, hc: Boolean) {
           }
