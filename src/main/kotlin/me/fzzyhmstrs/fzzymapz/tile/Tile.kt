f6c73d7ae1d29bce4b2ee7c2106e3f2d48e53339
fzzyhmstrs <72876796+fzzyhmstrs@users.noreply.github.com>
2023-05-19 11:23:33 -0400

    Update Tile.kt

--- src/main/kotlin/me/fzzyhmstrs/fzzymapz/tile/Tile.kt
+++ src/main/kotlin/me/fzzyhmstrs/fzzymapz/tile/Tile.kt
@@ -6,7 +6,7 @@ import me.fzzyhmstrs.fzzymapz.registry.RegisterTile
 import net.minecraft.client.util.math.MatrixStack
 import net.minecraft.util.Identifier
 
-abstract class Tile(val id: Identifier, val layer: MapLayer, val type: TileType, private val hasContent: Boolean = true){
+abstract class Tile(val id: Identifier, val layer: MapLayer, val type: TileType, val hasContent: Boolean = true){
     
     fun drawTile(matrices: MatrixStack, x: Int, y: Int, scale: Float, texture: Identifier){
         if(hasContent){
