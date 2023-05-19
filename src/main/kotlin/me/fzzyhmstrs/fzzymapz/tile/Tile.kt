c83d96b5fd28ad2d1c82228e23ddc4aa6298252c
fzzyhmstrs <72876796+fzzyhmstrs@users.noreply.github.com>
2023-05-19 09:49:34 -0400

    Update Tile.kt

--- src/main/kotlin/me/fzzyhmstrs/fzzymapz/tile/Tile.kt
+++ src/main/kotlin/me/fzzyhmstrs/fzzymapz/tile/Tile.kt
@@ -6,22 +6,22 @@ import me.fzzyhmstrs.fzzymapz.registry.RegisterTile
 import net.minecraft.client.util.math.MatrixStack
 import net.minecraft.util.Identifier
 
-abstract class Tile(val id: Identifier, val type: TileType, private val hasContent: Boolean = true){
-
-        fun drawTile(matrices: MatrixStack, x: Int, y: Int, theme: Theme){
-            if(hasContent){
-                draw(matrices, x, y, theme)
-            }
+abstract class Tile(val id: Identifier, val layer: MapLayer, val type: TileType, private val hasContent: Boolean = true){
+    
+    fun drawTile(matrices: MatrixStack, x: Int, y: Int, scale: Float, texture: Identifier){
+        if(hasContent){
+            draw(matrices, x, y, scale, texture)
         }
+    }
 
-      abstract fun draw(matrices: MatrixStack, x: Int, y: Int, theme: Theme)
-      
-      open fun toJson(): JsonObject {
-            return type.saveTile(this)
-      }
-      
-      object EMPTY: Tile(Identifier(FM.MOD_ID,"empty_tile"), RegisterTile.EmptyType, false){
-          override fun draw(matrices: MatrixStack, x: Int, y: Int) {
-          }
-      }
-  }
+    abstract fun draw(matrices: MatrixStack, x: Int, y: Int, scale: Float, texture: Identifier)
+
+    open fun toJson(): JsonObject {
+        return type.saveTile(this)
+    }
+
+    object EMPTY: Tile(Identifier(FM.MOD_ID,"empty_tile"), RegisterTile.EmptyType, false){
+        override fun draw(matrices: MatrixStack, x: Int, y: Int, scale: Float, texture: Identifier) {
+        }
+    }
+}
