ab7ffb3174419aa423cd722ee218699e84b13748
fzzyhmstrs <72876796+fzzyhmstrs@users.noreply.github.com>
2023-05-19 10:06:56 -0400

    Create TIleStack.kt

---
+++ src/main/kotlin/me/fzzyhmstrs/fzzymapz/tile/TIleStack.kt
@@ -0,0 +1,18 @@
+package me.fzzyhmstrs.fzzymapz.tile
+
+import com.google.gson.JsonObject
+import me.fzzyhmstrs.fzzymapz.FM
+import me.fzzyhmstrs.fzzymapz.registry.RegisterTile
+import net.minecraft.client.util.math.MatrixStack
+import net.minecraft.util.Identifier
+
+class TileStack(val tile: Tile, var scale: Float, var texture: Identifier){
+    fun updateTheme(theme: Theme){
+        this.scale = theme.getScale()
+        this.texture = theme.provide(tile.id)
+    }
+    
+    fun drawTile(matrices: MatrixStack, x: Int, y: Int){
+        tile.drawTile(matrices, x, y, scale, texture)
+    }
+}
